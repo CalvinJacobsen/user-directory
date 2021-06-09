2bacaf4462a7d9c76949cac3cde4681edf7aadee
Calvin Jacobsen <Jaco1932@umn.edu>
2021-06-09 18:13:30 -0500

    created components and edited app.js to include comps

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Employee Directory</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
